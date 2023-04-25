93d5649a66b8281fb874228325f0565145037bf8
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-04-25 00:44:47 -0400

    Solved Bonus

--- src/Form.js
+++ src/Form.js
@@ -5,31 +5,30 @@ function Form() {
   const [values, setValues] = useState("");
   const [operation, setOperation] = useState("");
   const [result, setResult] = useState("");
+  const [className, setClassName] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
     
     let numArr = values.split(",").map(Number);
     
-    if (!operation) {
+    if (!operation || !values || numArr.includes(NaN)) {
       setResult("Invalid input.");
+      setClassName("error");
     }
-    else if (operation === "sum") {
-      setResult(numArr.reduce((a, b) => a + b, 0));
-      setOperation("");
-      setValues("");
-    }
-    else if (operation === "average") {
-      setResult(numArr.reduce((a, b) => a + b, 0) / numArr.length);
-      setOperation("");
-      setValues("");
-    }
-    else if (operation === "mode") {
-      const mode = {};
-      numArr.forEach(num => mode[num] ? mode[num]++ : mode[num] = 1);
-      setResult(Object.keys(mode).reduce((a, b) => mode[a] > mode[b] ? a : b));
-      setOperation("");
-      setValues("");
+    else {
+      if (operation === "sum") {
+        setResult(numArr.reduce((a, b) => a + b, 0));
+      }
+      else if (operation === "average") {
+        setResult(numArr.reduce((a, b) => a + b, 0) / numArr.length);
+      }
+      else if (operation === "mode") {
+        const mode = {};
+        numArr.forEach(num => mode[num] ? mode[num]++ : mode[num] = 1);
+        setResult(Object.keys(mode).reduce((a, b) => mode[a] > mode[b] ? a : b));
+      }
+      resetValues();
     }
   }
 
@@ -41,11 +40,17 @@ function Form() {
     setOperation(event.target.value);
   }
 
+  function resetValues() {
+    setOperation("");
+    setValues("");
+    setClassName("");
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input id="values" name="values" type="text" value={values} onChange={handleInputChange} />
-        <select id="operation" name="operation" value={operation} onChange={handleOperation}>
+        <input className={className} id="values" name="values" type="text" value={values} onChange={handleInputChange} />
+        <select className={className} id="operation" name="operation" value={operation} onChange={handleOperation}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
