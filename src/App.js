a9508df7286883db57e8c6d803ba569bf421b5ea
Judy <judychue@pursuit.org>
2023-04-25 17:50:32 -0400

    first attempt will resubmit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+// import React from "react";
+// import Form from "./Form";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <main>
+//       <p>Enter each number in the array, separated by a ','</p>
+//       <Form />
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
 import Form from "./Form";
 import "./App.css";
--- src/Form.js
+++ src/Form.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form() {
+  const [values, setValues] = useState("");
+  const [operation, setOperation] = useState("");
+  const [result, setResult] = useState("");
+  const [className, setClassName] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    let numArr = values.split(",").map(Number);
+    
+    if (!operation || !values || numArr.includes(NaN)) {
+      setResult("Invalid input.");
+      setClassName("error");
+    }
+    else {
+      if (operation === "sum") {
+        setResult(numArr.reduce((a, b) => a + b, 0));
+      }
+      else if (operation === "average") {
+        setResult(numArr.reduce((a, b) => a + b, 0) / numArr.length);
+      }
+      else if (operation === "mode") {
+        
+        const mode = {};//missing stuff
+
+  function handleInputChange(event) {
+    setValues(event.target.value);
+  }
+
+  function handleOperation(event) {
+    setOperation(event.target.value);
+  }
+
+  function resetValues() {
+    setOperation("");
+    setValues("");
+    setClassName("");
+  }
+
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input className={className} id="values" name="values" type="text" value={values} onChange={handleInputChange} />
+        <select className={className} id="operation" name="operation" value={operation} onChange={handleOperation}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
+//missing stuff
+
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
