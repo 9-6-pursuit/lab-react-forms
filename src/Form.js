b661a2f5c7b09aeeb198ec7551863209a02490dc
Justin Knolley <justinknolley@pursuit.org>
2023-04-24 23:07:05 -0400

    React Forms

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,41 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
 function Form() {
+  const [values, setValues] = useState("")
+  const [operation, setOperation] = useState("")
+  const [result, setResult] = useState(null)
+
+  function handleValueInputChange(event) {
+    console.log(event.target.value)
+    setValues(event.target.value)
+  }
+
+  function operationChange(event) {
+    setOperation(event.target.value)
+  }
+
+  function formSubmit(event) {
+    event.preventDefault()
+    
+    let array = [values]
+    let result = 0
+    array.split(",")
+    for (const value of array) {
+      if (operation === "sum") {
+        result += value
+      } else if (operation === "average") {
+        result = value / array.length
+      }
+    }
+  }
+
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={formSubmit}>
+        <input id="values" name="values" type="text" onChange={handleValueInputChange} />
+        <select id="operation" name="operation" onChange={operationChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -15,7 +44,7 @@ function Form() {
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
