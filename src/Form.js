99c4ce8a294a4de52c71251f5b5983b0847c83d6
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-04-26 01:40:53 -0400

    modify react forms lab solution

--- src/Form.js
+++ src/Form.js
@@ -7,9 +7,11 @@ function Form() {
   const [values, setValues] = useState("")
   const [operation, setOperation] = useState("")
   const [result, setResult] = useState(null)
-  const [inputError, setInputError] = useState(false)
   const inputRef = useRef(null)
-
+  const selectRef = useRef(null)
+  const inputElement = document.getElementById("values")
+  const selectElement = document.getElementById("operation")
+  
   function handleValueInputChange(event) {
     setValues(event.target.value)
     // console.log(event.target.value)
@@ -22,60 +24,49 @@ function Form() {
   }
 
   function handleCalculate(event) {
+    
     event.preventDefault()
+    
+    
+    let inputValues = (values.split(",")).reduce((accumulator, value) => accumulator.concat(+value), [])
+    if (inputValues[0] === 0 || inputValues.some(isNaN) || selectElement.selectedIndex === 0) {
+      alert("Invalid input.")
+      inputValues = []
+      setValues("")
+      setOperation("")
 
-    // Update class of input and select elements based on inputError
-    const inputElement = document.getElementById("values")
-    if (inputError) {
+
+  if (inputValues[0] === 0 || inputValues.some(isNaN)) {
       inputElement.classList.add("error")
-    } else {
-      inputElement.classList.remove("error")
-    }
 
-    const selectElement = document.getElementById("operation")
-    if (inputError) {
+    } 
+
+    if (selectElement.selectedIndex === 0) {
       selectElement.classList.add("error")
-    } else {
-      selectElement.classList.remove("error")
+
+    } 
+
+      return
     }
 
-    if (!values.trim()) {
-      setResult(null);
-      setValues("");
-      setOperation("");
-      setInputError(true);
-      return;
-    }
-
+  
     // let inputValues = values.split(",")
     // let inputValues = values.split(",").map((val) => Number(val.trim()));
 
-    let inputValues = (values.split(",")).reduce((accumulator, value) => accumulator.concat(+value), [])
-    console.log(inputValues)
-
-    let result = null
-
-    if (inputValues.length === 0 || inputValues.some(isNaN)) {
-      alert("Invalid input.")
-      inputError = true
-    }
-    if (selectElement.selectedIndex === 0) {
-      alert("Invalid input.");
-    }
 
     // Operation
     if (operation === "sum") {
       let total = 0
       for (let element of inputValues) {
         total += element
-        result = total
+        setResult(total)
       }
 
     } else if (operation === "average") {
       let total = 0
       for (let element of inputValues) {
         total += element
-        result = (total / inputValues.length)
+        setResult(total / inputValues.length)
       }
 
     } else if (operation === "mode") {
@@ -93,18 +84,19 @@ function Form() {
           duplicateNum.push(Number(num));
         }
       }
-      result = duplicateNum
+      setResult(duplicateNum)
 
     }
-    setResult(result)
+
 
     // Clear the input box
     inputRef.current.value = ""
+    selectRef.current.value = ""
     setValues("")
-
-
+    setOperation("")
 
   }
+
 
   return (
     <>
@@ -117,7 +109,7 @@ function Form() {
           // pattern="[0-9]+(,[0-9]+)*" 
           ref={inputRef}
         />
-        <select id="operation" name="operation" onChange={handleOperationType}>
+        <select id="operation" name="operation" onChange={handleOperationType} ref={selectRef}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
