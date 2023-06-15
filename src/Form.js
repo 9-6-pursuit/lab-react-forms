291bf36e60f9bf188348e650c081cdfb97e2ffea
Jerry John <jerryjohn@jerrys-air.myfiosgateway.com>
2023-06-14 23:21:34 -0400

    Final Commit

--- src/Form.js
+++ src/Form.js
@@ -1,24 +1,91 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
 function Form() {
+  const [inputText, setInputText] = useState("");
+  const [operation, setOperation] = useState("sum");
+  const [result, setResult] = useState("");
+
+  function handleChange(event) {
+    const { id, value } = event.target;
+    if (id === "inputText") {
+      setInputText(value);
+    } else if (id === "operation") {
+      setOperation(value);
+    }
+  }
+
+  function calculate(event) {
+    event.preventDefault();
+    const numbers = inputText
+      .split(",")
+      .map((num) => num.trim())
+      .filter((num) => !isNaN(num))
+      .map((num) => parseInt(num, 10));
+
+    if (numbers.length === 0) {
+      setResult("Invalid input.");
+      return;
+    }
+
+    let result;
+    if (operation === "sum") {
+      result = numbers.reduce((acc, num) => acc + num, 0);
+    } else if (operation === "average") {
+      result = numbers.reduce((acc, num) => acc + num, 0) / numbers.length;
+    } else if (operation === "mode") {
+      const freqMap = {};
+      let maxFreq = 0;
+      let mode = numbers[0];
+
+      numbers.forEach((num) => {
+        if (freqMap[num]) {
+          freqMap[num]++;
+        } else {
+          freqMap[num] = 1;
+        }
+
+        if (freqMap[num] > maxFreq) {
+          maxFreq = freqMap[num];
+          mode = num;
+        }
+      });
+
+      result = mode;
+    } else {
+      result = "Invalid input.";
+    }
+
+    if (isNaN(result)) {
+      setResult("Invalid input.");
+    } else {
+      setResult(result);
+    }
+  }
+
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
+      <form onSubmit={calculate}>
+        <input
+          type="text"
+          id="inputText"
+          value={inputText}
+          onChange={handleChange}
+          placeholder="Enter comma-separated numbers"
+        />
+
+        <select id="operation" value={operation} onChange={handleChange}>
+          <option value="sum">Sum</option>
+          <option value="average">Average</option>
+          <option value="mode">Mode</option>
         </select>
-        <button type="submit">Calculate</button>
+        <button>Calculate</button>
       </form>
       <section id="result">
-        <p></p>
+        <p>{result}</p>
       </section>
     </>
   );
 }
-
 export default Form;
